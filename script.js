65b75c937f6ab380034a33f6a0876f1f125bc916
Marco <usechermarcoj@gmail.com>
2017-06-25 19:07:44 -0400

    portafolio version 1.2 con paginas de formacion. Problema del favicon

---
+++ script.js
@@ -0,0 +1,28 @@
+var css=document.getElementById('css');
+
+css.style.width= "60%";
+
+var sass=document.getElementById('sass');
+
+sass.style.width= "55%";
+
+var pug=document.getElementById('pug');
+
+pug.style.width= "55%";
+
+var js=document.getElementById('js');
+
+js.style.width= "50%";
+
+
+var git=document.getElementById('git');
+
+git.style.width= "65%";
+
+var photoshop=document.getElementById('photoshop');
+
+photoshop.style.width= "40%";
+
+var ingles= document.getElementById('ingles');
+
+ingles.style.width= "70%";
